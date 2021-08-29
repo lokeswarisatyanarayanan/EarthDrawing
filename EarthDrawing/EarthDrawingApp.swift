094d890b35773c7da7f4da24fda8c5547b69600d
Lokeswari Satyanarayana <lokeswari.satyanarayana@mutualmobile.com>
2021-08-29 15:42:13 +0530

    Initial Commit

---
+++ EarthDrawing/EarthDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthDrawingApp.swift
+//  EarthDrawing
+//
+//  Created by Lokeswari Satyanarayana on 29/08/21.
+//
+
+import SwiftUI
+
+@main
+struct EarthDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
